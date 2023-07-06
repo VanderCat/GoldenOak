7bb5d3d119957a085755f180afaa96883f297370
Влад Кот <vanderofcat@gmail.com>
2023-07-06 15:14:04 +0300

    [API] /authenticate: Mojang Complied Response

--- src/auth/login.lua
+++ src/auth/login.lua
@@ -6,8 +6,22 @@ local luasodium = require "luasodium"
 local auth = require "auth.shared"
 local config = require("lapis.config").get()
 
+--TODO: Rate limit: 3 operations/sec
+--                  More -> timeout for 10 seconds
 return function (request)
+    ---@type table<string, (string | nil)>
     local body = cjson.decode(request.req:read_body_as_string())
+
+    local checkResult, cause = auth.checks.username(body.username)
+    if checkResult then
+        checkResult, cause = auth.checks.password(body.password)
+    end
+    if not checkResult then
+        return errors.InvalidCredentials{
+            cause = cause
+        }
+    end
+
     local usersDb = config.db:getCollection('goldenoak', 'users')
     local user, err = usersDb:findOne{username=body.username}
     if not user then
@@ -18,5 +32,33 @@ return function (request)
     if not correctPassword then
         return errors.InvalidCredentials()
     end
-    return "success!"
+
+    local response = {}
+
+    local clientToken, newClientToken = body.clientToken, false
+    if not clientToken then
+        --TODO: Invalidate all tokens? not sure...
+        clientToken = uuid.generate()
+        newClientToken = true
+    end
+    response.clientToken = newClientToken and uuid.stringify(clientToken, true) or clientToken
+    
+    response.accessToken = "" --TODO: Generate
+
+    local profile = {
+        id = uuid.stringify(user.uuid[1], true),
+        name = user.username
+    }
+    response.availableProfiles = {profile}
+    response.selectedProfile = profile
+
+    if body.requestUser then
+        response.user = {
+            username = user.username, --TODO: use email
+            properties = cjson.empty_array, --TODO: user properties
+            id = "" -- TODO: wth is remoteID?????????????
+        }
+    end
+
+    return {json=response}
 end
